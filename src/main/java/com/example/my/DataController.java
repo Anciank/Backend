e36fb5a381fbb76dc11d3d0cef76fe519117b88d
sanmu-and-tian <2248495983@qq.com>
2023-11-23 23:03:04 +0800

    change json filepath

--- src/main/java/com/example/my/DataController.java
+++ src/main/java/com/example/my/DataController.java
@@ -1,5 +1,6 @@
 package com.example.my;
 
+import org.json.JSONArray;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +11,16 @@ import org.apache.calcite.model.JsonCustomSchema;
 import org.apache.calcite.model.JsonRoot;
 import org.apache.calcite.schema.SchemaPlus;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.Reader;
+import java.sql.*;
 
 import org.json.JSONObject;
+
+import org.json.JSONException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -25,10 +30,9 @@ public class DataController {
     public String sendDataToBackend(@RequestBody String data) throws Exception {
         // Process the received data (e.g., perform some business logic)
         // For this example, we'll simply return the received data + 1.
-
         // 加载模型文件
         Class.forName("org.apache.calcite.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:calcite:model=/Users/anciank/IdeaProjects/My/src/main/resources/model.json");
+        Connection connection = DriverManager.getConnection("jdbc:calcite:model=D:\\article\\Backend\\src\\main\\resources\\model.json");
         CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);
 
         // 获取根架构
@@ -46,19 +50,64 @@ public class DataController {
         String sql = sqlQuery;
         ResultSet resultSet = statement.executeQuery(sql);
 
+        StringBuilder resultStringBuilder = new StringBuilder();
 
-        String name = "";
-        // 处理查询结果
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
         while (resultSet.next()) {
-            // 处理查询结果行
-            name += resultSet.getString("NAME");
-            //int salary = resultSet.getInt("SALARY");
+            // Iterate through columns
+            for (int i = 1; i <= columnCount; i++) {
+                // Modify this part based on the actual types of your columns
+                String columnValue = resultSet.getString(i);
+                resultStringBuilder.append(columnValue).append(", ");
+            }
+            // Move to the next line for the next row
+            resultStringBuilder.append("\n");
         }
 
         resultSet.close();
         statement.close();
         connection.close();
 
-        return name;
+        return resultStringBuilder.toString();
     }
+
+    /*
+        修改csv文件路径,filePath为"{ "fp": "D:-----"}"
+    */
+
+    @PostMapping("/CJF")
+    public void ChangeJsonFile(@RequestBody String filePath) throws IOException, JSONException{
+        String fp = "D:\\article\\Backend\\src\\main\\resources\\model.json";
+        try (Reader reader = new FileReader(fp)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int c;
+            while ((c = reader.read()) != -1) {
+                stringBuilder.append((char) c);
+            }
+            JSONObject jsonObject =  new JSONObject(stringBuilder.toString());
+
+            JSONObject jsonObject1 = new JSONObject(filePath);
+
+            // 提取 "sql" 字段的值
+            String fileP = jsonObject1.getString("fp");
+
+            // 获取 "schemas" 数组
+            JSONArray schemas = jsonObject.getJSONArray("schemas");
+
+            // 获取 "operand" 对象
+            JSONObject operand = schemas.getJSONObject(0).getJSONObject("operand");
+
+            // 修改 "directory" 的值
+            operand.put("directory", fileP);
+
+            try (FileWriter writer = new FileWriter(fp)) {
+                writer.write(jsonObject.toString(2)); // 2是缩进的空格数，可以根据需要调整
+            }
+
+        }
+
+    }
+
 }
