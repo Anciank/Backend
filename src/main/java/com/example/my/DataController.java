055a29ca1a23260e10a8e6b446a907ac19ba7ae2
sanmu-and-tian <2248495983@qq.com>
2023-12-07 17:55:51 +0800

    2

--- src/main/java/com/example/my/DataController.java
+++ src/main/java/com/example/my/DataController.java
@@ -1,53 +1,48 @@
 package com.example.my;
 
+import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.json.JSONArray;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import org.apache.calcite.jdbc.CalciteConnection;
 import org.apache.calcite.model.JsonCustomSchema;
 import org.apache.calcite.model.JsonRoot;
 import org.apache.calcite.schema.SchemaPlus;
 
-import java.io.Reader;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.sql.*;
 
 import org.json.JSONObject;
 
 import org.json.JSONException;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.EnumSet;
 
 @RestController
 @RequestMapping("/api")
 public class DataController {
 
+    private final String fpath = "D:\\article\\Backend\\src\\main\\resources";
+
     @PostMapping("/sendDataToBackend")
     public String sendDataToBackend(@RequestBody String data) throws Exception {
-        // Process the received data (e.g., perform some business logic)
-        // For this example, we'll simply return the received data + 1.
-        // 加载模型文件
         Class.forName("org.apache.calcite.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:calcite:model=D:\\article\\Backend\\src\\main\\resources\\model.json");
+        String temp = "jdbc:calcite:model="+ this.fpath+"\\model.json";
+        Connection connection = DriverManager.getConnection(temp);
         CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);
+        Statement statement = connection.createStatement();
 
-        // 获取根架构
-        SchemaPlus rootSchema = calciteConnection.getRootSchema();
-
-        // Process input data
-        // 使用 org.json.JSONObject 解析 JSON 字符串
         JSONObject jsonObject = new JSONObject(data);
 
         // 提取 "sql" 字段的值
         String sqlQuery = jsonObject.getString("sql");
 
-        // 执行 SQL 查询
-        Statement statement = connection.createStatement();
         String sql = sqlQuery;
+
         ResultSet resultSet = statement.executeQuery(sql);
 
         StringBuilder resultStringBuilder = new StringBuilder();
@@ -73,41 +68,68 @@ public class DataController {
         return resultStringBuilder.toString();
     }
 
+//    保存文件
+    @PostMapping("/addDataset")
+    public ResponseEntity<String> handleFileUpload( @RequestParam("file") MultipartFile file){
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("文件不能为空");
+        }
+        // 设置保存文件的目录，可以根据实际需求修改
+        String uploadDir = this.fpath + "\\TempFile";
+
+        try {
+            // 获取文件的字节数组并保存到本地
+            byte[] bytes = file.getBytes();
+            Path filePath = Paths.get(uploadDir, file.getOriginalFilename());
+            Files.write(filePath, bytes);
+
+            // 返回成功的响应
+            return ResponseEntity.ok().body("{ \"ack\": true }");
+        } catch (IOException e) {
+            e.printStackTrace();
+            // 处理保存文件时的异常
+            return ResponseEntity.status(500).body("{ \"ack\": false, \"error\": \"文件保存失败\" }");
+        }
+    }
+
     /*
-        修改csv文件路径,filePath为"{ "fp": "D:-----"}"
+        修改数据文件
     */
 
-    @PostMapping("/CJF")
-    public void ChangeJsonFile(@RequestBody String filePath) throws IOException, JSONException{
-        String fp = "D:\\article\\Backend\\src\\main\\resources\\model.json";
-        try (Reader reader = new FileReader(fp)) {
-            StringBuilder stringBuilder = new StringBuilder();
-            int c;
-            while ((c = reader.read()) != -1) {
-                stringBuilder.append((char) c);
-            }
-            JSONObject jsonObject =  new JSONObject(stringBuilder.toString());
+    @PostMapping("/changeJsonFile")
+    public void changeJsonFile(@RequestBody String fileName) {
+        // 输入文件夹路径和输出文件夹路径，根据实际情况修改
+        String inputFolderPath = this.fpath + "\\TempFile";
+        String outputFolderPath = this.fpath + "\\csvFile";
 
-            JSONObject jsonObject1 = new JSONObject(filePath);
-
-            // 提取 "sql" 字段的值
-            String fileP = jsonObject1.getString("fp");
-
-            // 获取 "schemas" 数组
-            JSONArray schemas = jsonObject.getJSONArray("schemas");
-
-            // 获取 "operand" 对象
-            JSONObject operand = schemas.getJSONObject(0).getJSONObject("operand");
-
-            // 修改 "directory" 的值
-            operand.put("directory", fileP);
-
-            try (FileWriter writer = new FileWriter(fp)) {
-                writer.write(jsonObject.toString(2)); // 2是缩进的空格数，可以根据需要调整
-            }
-
+        try {
+            // 在输入文件夹中找到与给定文件名相同的文件
+            Files.walkFileTree(Path.of(inputFolderPath), EnumSet.noneOf(FileVisitOption.class), Integer.MAX_VALUE,
+                    new SimpleFileVisitor<Path>() {
+                        @Override
+                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                            String fileWithoutExtension = removeFileExtension(file.getFileName().toString());
+                            if (fileWithoutExtension.equals(fileName)) {
+                                // 清空输出文件夹
+                                FileUtils.cleanDirectory(new File(outputFolderPath));
+                                // 复制文件到输出文件夹
+                                Files.copy(file, Paths.get(outputFolderPath, file.getFileName().toString()), StandardCopyOption.REPLACE_EXISTING);
+                                // 找到文件后可以终止遍历
+                                return FileVisitResult.TERMINATE;
+                            }
+                            return FileVisitResult.CONTINUE;
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
+    }
+    private String removeFileExtension(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex != -1) {
+            return fileName.substring(0, lastDotIndex);
+        }
+        return fileName;
     }
 
 }
